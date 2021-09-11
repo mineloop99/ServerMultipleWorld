29cca6d27406a673620b920d77ce931a2dfdb38a
Mine Loop <84199166+mineloop99@users.noreply.github.com>
2021-09-11 16:55:07 +0700

    Add files via upload

---
+++ ServerMultipleWorld/GameLogic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerMultipleWorlds
+{
+    class GameLogic
+    {
+        public static void Update()
+        {
+            ThreadManager.UpdateMain();
+        }
+    }
+}
---
+++ ServerMultipleWorld/ServerHandle.cs
@@ -0,0 +1,24 @@
+﻿using GameServer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerMultipleWorlds
+{
+    class ServerHandle
+    {
+        public static void WelcomeReceived(int _fromClient, Packet _packet)
+        {
+            int _clientIdCheck = _packet.ReadInt();
+            string _username = _packet.ReadString();
+
+            Console.WriteLine($"{Server.clients[_fromClient].tcp.socket.Client.RemoteEndPoint} connect successfully and is now player {_fromClient}");
+            if (_fromClient != _clientIdCheck)
+            {
+                Console.WriteLine($"Player \"{_username}\" (ID: {_fromClient} has assumed the wrong client ID ({_clientIdCheck})!");
+            }
+            Server.clients[_fromClient].SentIntoGame(_username);
+            //TODO: send player to the game
+        }
+    }
+}
